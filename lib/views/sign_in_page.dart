a108f4f34f8bb9cbb4f7c2f6e661a37bf1715757
Erdem Saray <erdemsaray09@gmail.com>
2022-12-25 19:28:42 +0300

    character choose bug is fixed

--- lib/views/sign_in_page.dart
+++ lib/views/sign_in_page.dart
@@ -6,111 +6,124 @@ import '../core/constants/project_variables.dart';
 import '../core/widgets/custom_elevated_button.dart';
 import '../view_models.dart/sign_in_model.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
 
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+final TextEditingController _characterEditController = TextEditingController();
+
+class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     FocusNode focusNode = FocusNode();
     final TextEditingController _textEditingController = TextEditingController();
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
+
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/loginbackground.jpg'), fit: BoxFit.cover)),
+      child: Scaffold(
         backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-        title: const Text("Sign in"),
-      ),
-      body: GestureDetector(
-        onTap: () {
-          focusNode.unfocus();
-        },
-        child: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/loginbackground.jpg'), fit: BoxFit.cover)),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          title: const Text("Sign in"),
+        ),
+        body: GestureDetector(
+          onTap: () {
+            focusNode.unfocus();
+          },
           child: context.watch<SignInModel>().busy
               ? const Center(child: CircularProgressIndicator())
               : Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Select a Character",
-                        style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      SizedBox(
-                        height: 120,
-                        child: StreamBuilder<QuerySnapshot>(
-                          stream: Provider.of<SignInModel>(context).getCharacters(),
-                          builder: (context, snapshot) {
-                            return snapshot.hasData
-                                ? GridView.builder(
-                                    padding: EdgeInsets.zero,
-                                    scrollDirection: Axis.horizontal,
-                                    shrinkWrap: true,
-
-                                    //shrinkWrap: true,
-                                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                      mainAxisSpacing: 7,
-                                      crossAxisSpacing: 7,
-                                      mainAxisExtent: 120,
-                                      crossAxisCount: 1,
-                                    ),
-                                    itemCount: snapshot.data?.size,
-
-                                    //searchTerm.isEmpty ? snapshot.data!.docs.length : 3,
-
-                                    itemBuilder: (context, index) {
-                                      return Container(
-                                        height: 200,
-                                        width: 200,
-                                        child: IngredientWidget(
-                                          imageLink: snapshot.data?.docs[index]['imageLink'],
-                                        ),
-                                      );
-                                    },
-                                  )
-                                : const Center(child: CircularProgressIndicator());
-                          },
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: SafeArea(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Select a Character",
+                          style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 18,
+                        const SizedBox(
+                          height: 7,
                         ),
-                        child: TextField(
-                          focusNode: focusNode,
-                          controller: _textEditingController,
-                          style: const TextStyle(color: Colors.white),
-                          cursorColor: Colors.white,
-                          autofocus: false,
-                          decoration: const InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                              color: Colors.white,
-                            )),
-                            hintText: 'Select an Username',
-                            hintStyle: TextStyle(color: Color.fromARGB(255, 255, 253, 253)),
-                            contentPadding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                        SizedBox(
+                          height: 120,
+                          child: StreamBuilder<QuerySnapshot>(
+                            stream: Provider.of<SignInModel>(context).getCharacters(),
+                            builder: (context, snapshot) {
+                              return snapshot.hasData
+                                  ? GridView.builder(
+                                      padding: EdgeInsets.zero,
+                                      scrollDirection: Axis.horizontal,
+                                      shrinkWrap: true,
+
+                                      //shrinkWrap: true,
+                                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                                        mainAxisSpacing: 7,
+                                        crossAxisSpacing: 7,
+                                        mainAxisExtent: 120,
+                                        crossAxisCount: 1,
+                                      ),
+                                      itemCount: snapshot.data?.size,
+
+                                      //searchTerm.isEmpty ? snapshot.data!.docs.length : 3,
+
+                                      itemBuilder: (context, index) {
+                                        _characterEditController.addListener(() {
+                                          setState(() {});
+                                        });
+                                        return Container(
+                                          height: 200,
+                                          width: 200,
+                                          child: IngredientWidget(
+                                            imageLink: snapshot.data?.docs[index]['imageLink'],
+                                          ),
+                                        );
+                                      },
+                                    )
+                                  : const Center(child: CircularProgressIndicator());
+                            },
                           ),
                         ),
-                      ),
-                      CustomElevatedButton(
-                          buttonColor: Color.fromARGB(156, 23, 78, 188),
-                          buttonTextStyle: FontItems.boldTextInter20,
-                          buttonTitle: "Sign in",
-                          buttonMetod: () => Provider.of<SignInModel>(context, listen: false)
-                              .signIn(_textEditingController.text, characterList[0], context)),
-                      const SizedBox(
-                        height: 40,
-                      )
-                    ],
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 18,
+                          ),
+                          child: TextField(
+                            focusNode: focusNode,
+                            controller: _textEditingController,
+                            style: const TextStyle(color: Colors.white),
+                            cursorColor: Colors.white,
+                            autofocus: false,
+                            decoration: const InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                color: Colors.white,
+                              )),
+                              hintText: 'Select an Username',
+                              hintStyle: TextStyle(color: Color.fromARGB(255, 255, 253, 253)),
+                              contentPadding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                            ),
+                          ),
+                        ),
+                        CustomElevatedButton(
+                            buttonColor: Color.fromARGB(156, 23, 78, 188),
+                            buttonTextStyle: FontItems.boldTextInter20,
+                            buttonTitle: "Sign In",
+                            buttonMetod: () => Provider.of<SignInModel>(context, listen: false)
+                                .signIn(_textEditingController.text, characterList[0], context)),
+                        const SizedBox(
+                          height: 50,
+                        )
+                      ],
+                    ),
                   ),
                 ),
         ),
@@ -139,12 +152,10 @@ class _IngredientWidgetState extends State<IngredientWidget> {
       width: 200,
       child: ElevatedButton(
         onPressed: () {
-          if (characterList[0] != widget.imageLink) {
-            characterList[0] = widget.imageLink;
-          } else {
-            characterList[0] = '';
-          }
+          characterList.clear();
 
+          characterList.add(widget.imageLink);
+          _characterEditController.text = widget.imageLink;
           setState(() {});
         },
         style: ButtonStyle(
